d9d02699d2dfa3b1dc45296f562ab48d2af088b6
algoristy <princechauhan060101@gmail.com>
2023-08-20 16:29:07 +0530

    Update all changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/home";
+import MovieList from "./components/movieList/MovieList";
+import Movie from "./pages/movieDetail/Movie";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route
+            path="movies/:type"
+            element={
+              <h1>
+                <MovieList />
+              </h1>
+            }
+          ></Route>
+          <Route
+            path="search/movie/:query"
+            element={
+              <h1>
+                <MovieList />
+              </h1>
+            }
+          ></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
